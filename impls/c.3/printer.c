d1567c6e0939beae9ca8292b38a7e8cea214a20c
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-05-08 15:15:05 +0300

    replaced strcat with memcpy
    how the hell did it work without crashing?

--- impls/c.3/printer.c
+++ impls/c.3/printer.c
@@ -104,31 +104,33 @@ char *pr_list(List *list, bool print_readably)
 {
     if (list == NULL) return NULL;
     size_t cap = 256;
-    char *str = calloc(cap, sizeof(char));
+    char *str = malloc(sizeof(*str) * cap);
     str[0] = '(';
     size_t len = 1;
 
     struct Node *node = list->head;
     while (node) {
-        char *s = pr_str((MalDatum*) node->value, print_readably);
+        char *s = pr_str(node->value, print_readably);
         int slen = strlen(s);
+
         if (len + slen >= cap) {
             cap = slen + cap * 1.5;
             str = realloc(str, cap * sizeof(char));
         }
-        strcat(str, s);
+
+        memcpy(str + len, s, slen);
         len += slen;
-        strcat(str, " ");
-        len++;
+        str[len++] = ' ';
         free(s);
+
         node = node->next;
     }
     // if non-empty replace last redundant ' '
     if (len > 1) {
         len--;
     }
-    str[len] = ')';
-    str[len + 1] = '\0';
+    str[len++] = ')';
+    str[len] = '\0';
 
     return str;
 }
